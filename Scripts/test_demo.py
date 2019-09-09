ddd6355ddb08ff61d26231c313b8915900c4ca1c
Ray <huang_changhou@163.com>
2019-09-09 16:20:15 +0800

    'first'

---
+++ Scripts/test_demo.py
@@ -0,0 +1,27 @@
+# class TestLogin:
+#
+#     def test_one(self):
+#         print('first')
+#         assert 1
+#
+#     def test_two(self):
+#         print('two')
+#         assert 1
+#
+#     def test_three(self):
+#         print('three')
+#         assert 1
+
+class TestLogin:
+    def test_one(self):
+        print('first')
+        assert 1
+
+    def test_two(self):
+        print('two')
+        assert 1
+
+    def test_three(self):
+        print('three')
+        assert 1
+
